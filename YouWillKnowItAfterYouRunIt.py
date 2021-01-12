0b7af62a1f82542f82055cf74afbd19ff6f162fe
Hurrieam <77335318+Hurrieam@users.noreply.github.com>
2021-01-13 01:52:36 +0800

    Add files via upload

---
+++ YouWillKnowItAfterYouRunIt.py
@@ -0,0 +1,42 @@
+# YouWillKnowItAfterYouRunIt.py
+
+from turtle import *
+
+color("red")
+penup()
+goto(-400, 100)
+pendown()
+
+forward(100)
+goto(-350, 100)
+right(90)
+forward(200)
+right(90)
+forward(50)
+backward(100)
+penup()
+
+goto(-250, 100)
+left(90)
+pendown()
+forward(200)
+left(90)
+forward(100)
+penup()
+
+goto(-50, 0)
+left(90)
+pendown()
+circle(50)
+penup()
+
+goto(0, 100)
+left(200)
+pendown()
+forward(230)
+left(150)
+forward(230)
+penup()
+
+goto(150, 100)
+right(90)
