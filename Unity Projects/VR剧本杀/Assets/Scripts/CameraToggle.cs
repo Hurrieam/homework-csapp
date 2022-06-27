d2707493a59f3d10aafd87188c21d555451d16d1
Hurrieam Hu <1286499252@qq.com>
2022-06-27 23:47:52 +0800

    unity

---
+++ Unity Projects/VR剧本杀/Assets/Scripts/CameraToggle.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 用空格键切换摄像机（上帝角色控制）
+public class CameraToggle : MonoBehaviour
+{
+
+    public Camera[] Cameras;
+    private Camera CurrentCamera;
+    private float DepthOfCurrentCamera;
+    private int IndexOfCurrentCamera;
+    private float tempDepth;
+
+    // 获取当前摄像机
+    private Camera getCurrentCamera(Camera[] Cameras)
+    {
+        // 遍历所有摄像机，depth最大的就是当前“起作用”的摄像机
+        Camera maxCameraDepthOfCamera = Cameras[0];
+        for (int i = 0; i < 4; i++)
+        {
+            if (Cameras[i].depth > maxCameraDepthOfCamera.depth)
+            {
+                maxCameraDepthOfCamera = Cameras[i];
+            }
+        }
+        return maxCameraDepthOfCamera;
+    }
+
+    // 获取当前摄像机的depth
+    private float getDepthOfCurrentCamera(Camera[] Cameras)
+    {
+        // 遍历所有摄像机，depth最大的就是当前“起作用”的摄像机
+        Camera maxCameraDepthOfCamera = Cameras[0];
+        for (int i = 0; i < 4; i++)
+        {
+            if (Cameras[i].depth > maxCameraDepthOfCamera.depth)
+            {
+                maxCameraDepthOfCamera = Cameras[i];
+            }
+        }
+        return maxCameraDepthOfCamera.depth;
+    }
+
+    // 获取当前摄像机在摄像机数组中的角标（0为玩家1的摄像机，以此类推）
+    private int getIndexOfCurrentCamera(Camera[] Cameras)
+    {
+        // 遍历所有摄像机，depth最大的就是当前“起作用”的摄像机
+        Camera maxCameraDepthOfCamera = Cameras[0];
+        int i;
+        for (i = 0; i < 4; i++)
+        {
+            if (Cameras[i].depth > maxCameraDepthOfCamera.depth)
+            {
+                maxCameraDepthOfCamera = Cameras[i];
+            }
+        }
+        return i - 1;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        CurrentCamera = getCurrentCamera(Cameras);
+        DepthOfCurrentCamera = getDepthOfCurrentCamera(Cameras);
+        IndexOfCurrentCamera = getIndexOfCurrentCamera(Cameras);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            tempDepth = Cameras[0].depth;
+            for (int i = 0; i < Cameras.Length - 1; i++)
+            {
+                Cameras[i].depth = Cameras[i + 1].depth;
+            }
+            Cameras[Cameras.Length - 1].depth = tempDepth;
+            // 更新全局变量
+            Start();
+            // 遍历摄像机数组内的所有摄像机，将全部摄像机的Audio Listener关闭
+            for (int i = 0; i < Cameras.Length; i++)
+            {
+                Cameras[i].GetComponent<AudioListener>().enabled = false;
+            }
+            // 打开当前摄像机的Audio Listener
+            CurrentCamera.GetComponent<AudioListener>().enabled = true;
+        }
+    }
+}
---
+++ Unity Projects/VR剧本杀/Assets/Scripts/ViewControl_Mouse.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 用鼠标控制视角（上帝角色控制）（模拟VR眼镜）
+// 分别挂载给每个摄像机（有几个挂几个）
+public class ViewControl_Mouse : MonoBehaviour
+{
+
+    private enum RotationAxes
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+    private RotationAxes m_axes = RotationAxes.MouseXAndY;
+
+    // 两个方向转向的灵敏度
+    private float m_sensitivityX = 1.5f;
+    private float m_sensitivityY = 1.5f;
+
+    // 水平方向的镜头转向
+    private float m_minimumX = -360f;
+    private float m_maximumX = 360f;
+    // 垂直方向的镜头转向 (这里给个限度 最大仰角为45°)
+    private float m_minimumY = -45f;
+    private float m_maximumY = 45f;
+
+    float m_rotationY = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        // 防止刚体影响镜头旋转
+        if (GetComponent<Rigidbody>())
+        {
+            GetComponent<Rigidbody>().freezeRotation = true;
+        }
+        // 隐藏鼠标光标
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_axes == RotationAxes.MouseXAndY)
+        {
+            float m_rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * m_sensitivityX;
+            m_rotationY += Input.GetAxis("Mouse Y") * m_sensitivityY;
+            m_rotationY = Mathf.Clamp(m_rotationY, m_minimumY, m_maximumY);
+
+            transform.localEulerAngles = new Vector3(-m_rotationY, m_rotationX, 0);
+        }
+        else if (m_axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * m_sensitivityX, 0);
+        }
+        else
+        {
+            m_rotationY += Input.GetAxis("Mouse Y") * m_sensitivityY;
+            m_rotationY = Mathf.Clamp(m_rotationY, m_minimumY, m_maximumY);
+
+            transform.localEulerAngles = new Vector3(-m_rotationY, transform.localEulerAngles.y, 0);
+        }
+    }
+}
